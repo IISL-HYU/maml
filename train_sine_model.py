b0d27cdfe88c0f8888365a9bf982db0f418a8811
S.W Hong <hongsw94@hanyang.ac.kr>
2022-09-10 00:34:01 +0900

    initial commit (lots of errors)

---
+++ train_sine_model.py
@@ -0,0 +1,286 @@
+# -*- coding: UTF-8 -*-
+import os
+import sys
+import random
+import numpy as np
+import tensorflow as tf 
+import time
+import matplotlib.pyplot as plt
+from sinusoid_generator import SinusoidGenerator, generate_dataset
+from sine_model import SineModel
+
+
+tf.keras.backend.set_floatx('float64')
+
+
+
+def np_to_tensor(list_of_numpy_objs):
+    return (tf.convert_to_tensor(obj) for obj in list_of_numpy_objs)
+
+def copy_model(model, x):
+    copied_model = SineModel()
+    copied_model.forward(x)
+    copied_model.set_weights(model.get_weights())
+    return copied_model
+
+def loss_fn(y, pred_y):
+    return tf.reduce_mean(tf.keras.metrics.mean_squared_error(y, pred_y))
+
+def compute_loss(model, x, y , loss_fn=loss_fn):
+    logits = model.forward(x)
+    mse = loss_fn(logits, y)
+    return mse, logits
+
+def compute_gradients(model, x, y, loss_fn=loss_fn):
+    with tf.GradientTape() as tape:
+        loss, logits = compute_loss(model, x, y, loss_fn)
+    return tape.gradient(loss, model.trainable_variables), loss
+
+def apply_gradients(optimizer, gradients, variables):
+    optimizer.apply_gradients(zip(gradients, variables))
+
+def train_step(x, y, model, optimizer):
+    tensor_x, tensor_y = np_to_tensor((x, y))
+    gradients, loss = compute_gradients(model, tensor_x, tensor_y)
+    apply_gradients(optimizer, gradients, model.trainable_variables)
+    return loss
+
+def regular_train(model, train_ds, epochs=1, lr=0.001, log_steps=1000):
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+    for eopch in range(epochs):
+        losses = []
+        total_loss = 0
+        start = time.time()
+        for i, sinusoid_generator in enumerate(train_ds):
+            x, y = sinusoid_generator.batch()
+            loss = train_step(x, y, model, optimizer)
+            total_loss += loss
+            curr_loss = total_loss / (i + 1.0)
+            losses.append(curr_loss)
+            
+            if i % log_steps == 0 and i > 0:
+                print('Step {}: loss = {}, Time to run {} steps = {:.2f} seconds'.format(
+                    i, curr_loss, log_steps, time.time() - start))
+                start = time.time()
+        plt.plot(losses)
+        plt.title('ANN loss per epoch')
+        plt.show()
+    return model
+
+def maml_train(model, train_ds, epochs=1, lr_inner=0.01, batch_size=1, log_steps=1000):
+    optimizer = tf.keras.optimizers.Adam()
+    for epoch in range(epochs):
+        total_loss = 0
+        losses = []
+        start = time.time()
+        # 打乱生成的训练集
+        for i, ds in enumerate(random.sample(train_ds, len(train_ds))):
+            x, y = np_to_tensor(ds.batch())
+            model.forward(x)
+            with tf.GradientTape() as test_tape:
+                with tf.GradientTape() as train_tape:
+                    train_loss, _ = compute_loss(model, x, y)
+                gradients = train_tape.gradient(train_loss, model.trainable_variables)
+                k=0
+                model_copy = copy_model(model, x)
+                for j in range(len(model_copy.layers)):
+                    model_copy.layers[j].kernel = tf.subtract(model.layers[j].kernel,
+                                tf.multiply(lr_inner, gradients[k]))
+                    model_copy.layers[j].bias = tf.subtract(model.layers[j].bias,
+                                tf.multiply(lr_inner, gradients[k+1]))
+                    k+=2
+                test_loss, logits = compute_loss(model_copy, x, y)
+            gradients = test_tape.gradient(test_loss, model.trainable_variables)
+            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        
+            # Logs
+            total_loss += test_loss
+            loss = total_loss / (i+1.0)
+            losses.append(loss)
+            
+            if i % log_steps == 0 and i > 0:
+                print('Step {}: loss = {}, Time to run {} steps = {}'.format(i, loss, log_steps, time.time() - start))
+                start = time.time()
+        plt.plot(losses)
+        plt.title('MAML losses per epoch')
+        plt.show()
+    return model
+
+def plot_model_comparison_to_average(model, ds, model_name='ANN', K=5):
+    '''Compare model to average.
+    
+    Computes mean of training sine waves actual `y` and compare to
+    the model's prediction to a new sine wave, the intuition is that
+    these two plots should be similar.
+    '''
+    sinu_generator = SinusoidGenerator(K=K)
+    
+    # calculate average prediction
+    avg_pred = []
+    for i, sinusoid_generator in enumerate(ds):
+        x, y = sinusoid_generator.equally_spaced_samples()
+        avg_pred.append(y)
+    
+    x, _ = sinu_generator.equally_spaced_samples()    
+    avg_plot, = plt.plot(x, np.mean(avg_pred, axis=0), '--')
+
+    # calculate model prediction
+    model_pred = model.forward(tf.convert_to_tensor(x))
+    model_plot, = plt.plot(x, model_pred.numpy())
+    
+    # plot
+    plt.legend([avg_plot, model_plot], ['Average', model_name])
+    plt.title(model_name)
+    plt.show()
+                
+
+def eval_sine_test(model, optimizer, x, y, x_test, y_test, num_steps=(0, 1, 10)):
+    '''Evaluate how the model fits to the curve training for `fits` steps.
+    
+    Args:
+        model: Model evaluated.
+        optimizer: Optimizer to be for training.
+        x: Data used for training.
+        y: Targets used for training.
+        x_test: Data used for evaluation.
+        y_test: Targets used for evaluation.
+        num_steps: Number of steps to log.
+    '''
+    fit_res = []
+    
+    tensor_x_test, tensor_y_test = np_to_tensor((x_test, y_test))
+    
+    # If 0 in fits we log the loss before any training
+    if 0 in num_steps:
+        loss, logits = compute_loss(model, tensor_x_test, tensor_y_test)
+        fit_res.append((0, logits, loss))
+        
+    for step in range(1, np.max(num_steps) + 1):
+        train_step(x, y, model, optimizer)
+        loss, logits = compute_loss(model, tensor_x_test, tensor_y_test)
+        if step in num_steps:
+            fit_res.append(
+                (
+                    step, 
+                    logits,
+                    loss
+                )
+            )
+    return fit_res
+
+
+def eval_sinewave_for_test(model, sinusoid_generator=None, num_steps=(0, 1, 10), lr=0.01, plot=True, name=None):
+    '''Evaluates how the sinewave addapts at dataset.
+    
+    The idea is to use the pretrained model as a weight initializer and
+    try to fit the model on this new dataset.
+    
+    Args:
+        model: Already trained model.
+        sinusoid_generator: A sinusoidGenerator instance.
+        num_steps: Number of training steps to be logged.
+        lr: Learning rate used for training on the test data.
+        plot: If plot is True than it plots how the curves are fitted along
+            `num_steps`.
+    
+    Returns:
+        The fit results. A list containing the loss, logits and step. For
+        every step at `num_steps`.
+    '''
+    
+    if sinusoid_generator is None:
+        sinusoid_generator = SinusoidGenerator(K=5)
+        
+    # generate equally spaced samples for ploting
+    x_test, y_test = sinusoid_generator.equally_spaced_samples(100)
+    
+    # batch used for training
+    x, y = sinusoid_generator.batch()
+    
+    # copy model so we can use the same model multiple times
+    copied_model = copy_model(model, x)
+    
+    # use SGD for this part of training as described in the paper
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
+    
+    # run training and log fit results
+    fit_res = eval_sine_test(copied_model, optimizer, x, y, x_test, y_test, num_steps)
+    
+    # plot
+    train, = plt.plot(x, y, '^')
+    ground_truth, = plt.plot(x_test, y_test)
+    plots = [train, ground_truth]
+    legend = ['trained', 'label']
+    for n, res, loss in fit_res:
+        cur, = plt.plot(x_test, res[:, 0], '--')
+        plots.append(cur)
+        legend.append(f'step {n}')
+    plt.legend(plots, legend)
+    plt.ylim(-5, 5)
+    plt.xlim(-6, 6)
+    plt.title(name)
+    if plot:
+        plt.show()
+    
+    return fit_res
+
+def compare_maml_and_neural_net(maml, neural_net, sinusoid_generator, num_steps=list(range(10)),
+                                intermediate_plot=True, marker='x', linestyle='--',figure_name=None):
+    '''Compare the loss of a MAML model and a neural net.
+    
+    Fits the models for a new task (new sine wave) and then plot
+    the loss of both models along `num_steps` interactions.
+    
+    Args:
+        maml: An already trained MAML.
+        neural_net: An already trained neural net.
+        num_steps: Number of steps to be logged.
+        intermediate_plot: If True plots intermediate plots from
+            `eval_sinewave_for_test`.
+        marker: Marker used for plotting.
+        linestyle: Line style used for plotting.
+    '''
+    if intermediate_plot:
+        print('MAML')
+    fit_maml = eval_sinewave_for_test(maml, sinusoid_generator, plot=intermediate_plot,name=figure_name)
+    if intermediate_plot:
+        print('Neural Net')
+    fit_neural_net = eval_sinewave_for_test(neural_net, sinusoid_generator, plot=intermediate_plot,name=figure_name)
+    
+    fit_res = {'MAML': fit_maml, 'ANN': fit_neural_net}
+    
+    legend = []
+    for name in fit_res:
+        x = []
+        y = []
+        for n, _, loss in fit_res[name]:
+            x.append(n)
+            y.append(loss)
+        plt.plot(x, y, marker=marker, linestyle=linestyle)
+        plt.xticks(num_steps)
+        legend.append(name)
+    plt.title('Compare MAML and ANN')
+    plt.legend(legend)
+    plt.show()
+
+
+if __name__ == '__main__':
+    model = SineModel()
+    train_ds, test_ds = generate_dataset(K=5)
+    name = 'neural network K=5 lr=0.001'
+    neural_model = regular_train(model, train_ds)
+    plot_model_comparison_to_average(neural_model, train_ds, model_name='comparison')
+    for index in np.random.randint(0, len(test_ds), size=3):
+        eval_sinewave_for_test(neural_model, test_ds[index],name=name)
+
+
+    # model = SineModel()
+    name = 'MAML K=5, Alpha=0.01 Beta=0.001'
+    maml_model = maml_train(model, train_ds)
+    plot_model_comparison_to_average(maml_model, train_ds, model_name='MAML')
+    for index in np.random.randint(0, len(test_ds), size=3):
+        eval_sinewave_for_test(maml_model, test_ds[index],name=name)
+
+    # for _ in range(3):
+    #     index = np.random.choice(range(len(test_ds)))
+    #     compare_maml_and_neural_net(maml_model, neural_model, test_ds[index])
